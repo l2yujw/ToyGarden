fd1cd249fa3808ab7e5235f84a826d15c87e0174
ryu <jeungwon28@gmail.com>
2024-04-02 15:03:05 +0900

    Initialize

---
+++ ryuboard/src/main/java/com/toygarden/ryuboard/RyuboardApplication.java
@@ -0,0 +1,13 @@
+package com.toygarden.ryuboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyuboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyuboardApplication.class, args);
+	}
+
+}
